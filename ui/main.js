61d490165eadfb7cf5fa17569280c53d0ae68347
imdhruvit <30293984+imdhruvit@users.noreply.github.com>
2017-08-19 23:23:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element=document.getElementById('main-text');
 element.innerHTML='New Value';
 //Move Image
 
-var img=document.getElemenrById('move');
+var img=document.getElementById('move');
 img.onclick = function() {
     img.style.marginLeft='500px';
 };
